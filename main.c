a68ef034901012a6328034018ffbb1426ab2f23b
htet-29 <htethlaingwin.bsn29@gmail.com>
2024-08-25 23:19:26 +0630

    installing curses.h header

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
-#include <conio.h>
+#include <curses.h>
 #include <stdio.h>
 
 int main()
 {
-    
+    initscr();            // Initialize the window
+    printw("Hello, World!"); // Print Hello, World!
+    refresh();            // Print it on the real screen
+    getch();              // Wait for user input
+    endwin();             // End curses mode
     return 0;
 }
